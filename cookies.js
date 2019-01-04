f808065fa01e97e17bbaa17738de22e4528907e4
Marcela Gomez <marcela.gomezdl@gmail.com>
2019-01-04 16:52:35 -0500

    added new js background to homepage

--- cookies.js
+++ cookies.js
@@ -1,10 +1,38 @@
 $("#btn").on("click", function(){
 	var user = $("#user").val()
 	Cookies.set ("usercookie", user)
+	var userpass = $("#pass").val()
+	Cookies.set ("userpass", userpass)
+	var usermail = $("#mail").val()
+	Cookies.set ("usermail", usermail)
+	
+	if(Cookies.get("usercookie") == "lucy"){
+	$("body").css("background-color", "pink")
+	$("#message").css("font-size", "100px")
+	$("#message").html("Welcome, " + Cookies.get("usercookie") + "! " + "Your password, " + Cookies.get("userpass") + "  is not great..")
+
+}
+
+	if(Cookies.get("usercookie") == "mike"){
+	$("body").css("background-color", "skyblue")
+	$("#message").html("Welcome, " + Cookies.get("usercookie") + "!" + " Your email is " + Cookies.get("usermail"))
+
+}
+
+
+
 })
 
-$("#message").html("Welcome, " + Cookies.get("usercookie"))
+$("#message").html("Welcome, " + Cookies.get("usercookie") + "Your password, " + Cookies.get("userpass") + " is not great..")
 
 if(Cookies.get("usercookie") == "lucy"){
 	$("body").css("background-color", "pink")
 }
+
+if(Cookies.get("usercookie") == "mike"){
+	$("body").css("background-color", "skyblue")
+}
+
+var d = new Date()
+var dd = String(d).slice(0,16)
+$("#date").html(dd)
